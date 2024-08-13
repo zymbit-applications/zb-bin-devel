4dd27cde8c1ff05d23383ba6666a5ba3f058c008
Ryan Wenger <ryan.wenger@zymbit.com>
2024-08-13 12:14:52 -0700

    clean up installer bootstrap script

--- install.sh
+++ install.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-echo "bootstrapping the zbcli installer. you may be asked for your password."
-pushd "$(mktemp -d)" >/dev/null
-curl -sSL https://github.com/zymbit-applications/zb-bin-devel/releases/download/installer/installer -o ./zb-installer
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "zb-install.sh: error: this script must run as root" >&2
+  exit 1
+fi
+
+echo "zb-install.sh: bootstrapping the zbcli installer"
+TMPDIR="$(mktemp -d)"
+
+function cleanup() {
+  echo "zb-install.sh: cleaning up"
+  popd >/dev/null 2>&1 || true
+  rm -rf "$TMPDIR"
+  exit
+}
+
+trap cleanup EXIT
+pushd "$TMPDIR" >/dev/null 2>&1 || true
+curl -sSL https://github.com/zymbit-applications/zb-bin/releases/download/installer/installer -o ./zb-installer
 chmod +x zb-installer
-sudo ./zb-installer $@
-rm zb-installer
-popd >/dev/null
+./zb-installer "$@"
