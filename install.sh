6bfef987e17d9281671f52878d2b12d829994569
Ryan Wenger <ryan.wenger@zymbit.com>
2024-08-13 10:54:37 -0700

    initial setup (configure repo URL & build workflow)

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "bootstrapping the zbcli installer. you may be asked for your password."
 pushd "$(mktemp -d)" >/dev/null
-curl -sSL https://github.com/zymbit-applications/zb-bin/releases/download/installer/installer -o ./zb-installer
+curl -sSL https://github.com/zymbit-applications/zb-bin-devel/releases/download/installer/installer -o ./zb-installer
 chmod +x zb-installer
 sudo ./zb-installer $@
 rm zb-installer
