936c2d24dda41793adfb623a6e53816be2f0f7a8
Ryan Wenger <ryan.wenger@zymbit.com>
2025-03-13 14:40:20 -0700

    fix version tag filtering logic to support handling of release candidates

--- src/toolchain/version.rs
+++ src/toolchain/version.rs
@@ -3,7 +3,11 @@ use octocrab::models::repos::Release;
 use urlencoding::encode;
 
 /// `tag_prefix`: "zbcli" in "zbcli-1.1.0"
-pub async fn list(tag_prefix: &str, zb_version: &Option<String>) -> Result<Vec<Release>> {
+pub async fn list(
+    tag_prefix: &str,
+    zb_version: &Option<String>,
+    n_items: u8,
+) -> Result<Vec<Release>> {
     let github_instance = octocrab::instance();
     #[cfg(feature = "zbcli-devel")]
     let repo = github_instance.repos("zymbit-applications", "zb-bin-devel");
@@ -19,28 +23,47 @@ pub async fn list(tag_prefix: &str, zb_version: &Option<String>) -> Result<Vec<R
                 .await
                 .context("Failed to get latest release")?
         } else {
-            let version_tag = encode(format!("{tag_prefix}-{version}").as_str()).into_owned();
+            let version_tag = encode(version).into_owned();
             releases
                 .get_by_tag(version_tag.as_str())
                 .await
-                .context(format!(
-                    "Failed to get release {version}.\n\
-                    Note that 'zbcli-' should be ommitted from the version argument;\n\
-                    e.g. to select 'zbcli-1.2.0-rc.23', specify '1.2.0-rc.23' as \
-                    the parameter to '--zb-version'"
-                ))?
+                .context(format!("Failed to get release tagged with '{version}'.\n"))?
         };
         std::iter::once(release).collect()
     } else {
-        releases
-            .list()
-            .per_page(10)
-            .send()
-            .await
-            .context("Failed to get latest releases")?
-            .into_iter()
-            .filter(|release| release.tag_name.starts_with(&format!("{tag_prefix}-")))
-            .collect()
+        let mut count = 0usize;
+        let mut page = 1u32;
+        let mut accumulate = Vec::new();
+        while count < (n_items as usize) {
+            accumulate.extend(
+                releases
+                    .list()
+                    .per_page(n_items)
+                    .page(page)
+                    .send()
+                    .await
+                    .map(|page|
+                        page.into_iter()
+                    ).or_else(|e|
+                        // don't bail if we get no results back unless we haven't accumulated any
+                        // from previous pages
+                        if accumulate.is_empty() {
+                            Err(e)
+                        } else {
+                            Ok(Vec::new().into_iter())
+                        })
+                    .context("Failed to get latest releases")?
+                    .filter(|release| release.tag_name.starts_with(tag_prefix)
+                                && !release.tag_name.contains("rc")), // exclude release candidates
+            );
+            if count == accumulate.len() {
+                break;
+            }
+            count = accumulate.len();
+            page += 1;
+        }
+        accumulate.truncate(n_items as usize);
+        accumulate
     };
 
     Ok(release_list)
